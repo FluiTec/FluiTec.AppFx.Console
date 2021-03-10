7b40a88505a823133790535925731744832c60bd
Achim Schnell <a.schnell@wtschnell.de>
2021-03-10 15:33:04 +0100

    init solution

--- README.md
+++ README.md
@@ -2,11 +2,5 @@
 
 ## AppFx ##
 
-#### Data ####
-* FluiTec.AppFx.Data : simplify common data-tasks
-
-### Build ###
-[![Build status](https://ci.appveyor.com/api/projects/status/ccumjouy0377c30c?svg=true)](https://ci.appveyor.com/project/IInvocation/fluitec-appfx-data)
-
-### Nuget ###
-[![Nuget version](https://img.shields.io/nuget/v/FluiTec.AppFx.Data.svg)](https://www.nuget.org/packages/FluiTec.AppFx.Data)
+#### Console ####
+* FluiTec.AppFx.Console : simplify common console-tasks
---
+++ src/SimpleSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SimpleSample
+{
+    /// <summary>   A program. </summary>
+    class Program
+    {
+        /// <summary>   Main entry-point for this application. </summary>
+        /// <param name="args"> An array of command-line argument strings. </param>
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            foreach (var arg in args)
+                Console.WriteLine($"- {arg}");
+        }
+    }
+}
