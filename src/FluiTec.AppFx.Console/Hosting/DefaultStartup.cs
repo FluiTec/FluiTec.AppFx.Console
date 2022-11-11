53dfdf3e28030c49df1e31dbf20d910a4c8452e2
IInvocation <a.schnell@wtschnell.de>
2022-11-11 09:40:44 +0100

    redo

---
+++ src/FluiTec.AppFx.Console/Hosting/DefaultStartup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FluiTec.AppFx.Console.Hosting
+{
+    /// <summary>
+    /// A default startup.
+    /// </summary>
+    public abstract class DefaultStartup : IStartup
+    {
+        /// <summary>
+        /// Configures the given builder.
+        /// </summary>
+        ///
+        /// <param name="context">  The context. </param>
+        /// <param name="builder">  The builder. </param>
+        public virtual void Configure(HostBuilderContext context, IConfigurationBuilder builder)
+        {
+            builder.AddJsonFile("appsettings.secret.json", true, true);
+        }
+
+        /// <summary>
+        /// Configure services.
+        /// </summary>
+        ///
+        /// <param name="context">  The context. </param>
+        /// <param name="services"> The services. </param>
+        public abstract void ConfigureServices(HostBuilderContext context, IServiceCollection services);
+    }
+}
---
+++ src/FluiTec.AppFx.Console/Hosting/IStartup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FluiTec.AppFx.Console.Hosting
+{
+    /// <summary>
+    /// Interface for startup.
+    /// </summary>
+    public interface IStartup
+    {
+        /// <summary>
+        /// Configures the given builder.
+        /// </summary>
+        ///
+        /// <param name="context">  The context. </param>
+        /// <param name="builder">  The builder. </param>
+        void Configure(HostBuilderContext context, IConfigurationBuilder builder);
+
+        /// <summary>
+        /// Configure services.
+        /// </summary>
+        ///
+        /// <param name="context">  The context. </param>
+        /// <param name="services"> The services. </param>
+        void ConfigureServices(HostBuilderContext context, IServiceCollection services);
+    }
+}
